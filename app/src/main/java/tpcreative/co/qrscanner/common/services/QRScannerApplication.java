0ff3303b863dab4e4ec7733483a22b78106e7e97
Mr Phong <ttphong@ecpay.vn>
2020-04-21 10:26:11 +0700

    [release-product-v1.5.0] - Updated pattern

--- app/src/main/java/tpcreative/co/qrscanner/common/services/QRScannerApplication.java
+++ app/src/main/java/tpcreative/co/qrscanner/common/services/QRScannerApplication.java
@@ -54,7 +54,7 @@ public class QRScannerApplication extends MultiDexApplication implements Depende
         Fabric.with(this, new Crashlytics());
         InstanceGenerator.getInstance(this);
         isLive = false;
-        if (!BuildConfig.BUILD_TYPE.equals(getResources().getString(R.string.release))) {
+        if (!Utils.isRelease()) {
             MobileAds.initialize(this, new OnInitializationCompleteListener() {
                 @Override
                 public void onInitializationComplete(InitializationStatus initializationStatus) {
