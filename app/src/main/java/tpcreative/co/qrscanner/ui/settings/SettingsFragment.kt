08278507c462e736b1af8cdb0d65761098319a2a
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-04-11 13:19:00 +0700

    [feature-151723122020] - Added in app purchaser

--- app/src/main/java/tpcreative/co/qrscanner/ui/settings/SettingsFragment.kt
+++ app/src/main/java/tpcreative/co/qrscanner/ui/settings/SettingsFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import co.tpcreative.supersafe.common.controller.EncryptedPreferenceDataStore
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.journeyapps.barcodescanner.BarcodeEncoder
@@ -451,6 +452,9 @@ class SettingsFragment : BaseFragment() {
         }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            if (QRScannerApplication.getInstance().isLiveMigration()){
+                preferenceManager.preferenceDataStore = EncryptedPreferenceDataStore.getInstance(requireContext())
+            }
             addPreferencesFromResource(R.xml.pref_general)
         }
 
