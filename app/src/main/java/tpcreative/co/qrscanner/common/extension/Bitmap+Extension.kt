32c3b023c079d0f2cbea3a47fcb4fe9d064e618e
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2022-12-09 23:21:32 +0700

    [feature-233705122022] - Improved address book Fixed load book address, Fixed null Fixed multiple display Fixed suffixes name Fixed display full name Added noted and birthday for mecard Change clipboard to copy Rename Added noted Fixed dark theme Fixed book address Improving csv Changed resolution for export image Improved location Improved display Improved export and print Fixed changed variable Fixed sync favorite and noted Fixed display code Fixed crack app

--- app/src/main/java/tpcreative/co/qrscanner/common/extension/Bitmap+Extension.kt
+++ app/src/main/java/tpcreative/co/qrscanner/common/extension/Bitmap+Extension.kt
@@ -6,38 +6,54 @@ import android.graphics.Color
 import tpcreative.co.qrscanner.ui.review.ReviewActivity
 
 fun Bitmap.addPaddingTopForBitmap(paddingTop: Int): Bitmap? {
-    val outputBitmap =
-        Bitmap.createBitmap(width, height + paddingTop, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(outputBitmap)
-    canvas.drawColor(Color.TRANSPARENT)
-    canvas.drawBitmap(this, 0F, paddingTop.toFloat(), null)
-    return outputBitmap
+    try {
+        val outputBitmap =
+            Bitmap.createBitmap(width, height + paddingTop, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(outputBitmap)
+        canvas.drawColor(Color.TRANSPARENT)
+        canvas.drawBitmap(this, 0F, paddingTop.toFloat(), null)
+        return outputBitmap
+    }catch (e : Exception){
+        return null
+    }
 }
 
 fun Bitmap.addPaddingBottomForBitmap(paddingBottom: Int): Bitmap? {
-    val outputBitmap =
-        Bitmap.createBitmap(width, height + paddingBottom, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(outputBitmap)
-    canvas.drawColor(Color.TRANSPARENT)
-    canvas.drawBitmap(this, 0F, 0F, null)
-    return outputBitmap
+    try {
+        val outputBitmap =
+            Bitmap.createBitmap(width, height + paddingBottom, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(outputBitmap)
+        canvas.drawColor(Color.TRANSPARENT)
+        canvas.drawBitmap(this, 0F, 0F, null)
+        return outputBitmap
+    }catch (e : Exception){
+        return null
+    }
 }
 
 
 fun Bitmap.addPaddingRightForBitmap(paddingRight: Int): Bitmap? {
-    val outputBitmap =
-        Bitmap.createBitmap(width + paddingRight, height, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(outputBitmap)
-    canvas.drawColor(Color.TRANSPARENT)
-    canvas.drawBitmap(this, 0F, 0F, null)
-    return outputBitmap
+    return try {
+        val outputBitmap =
+            Bitmap.createBitmap(width + paddingRight, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(outputBitmap)
+        canvas.drawColor(Color.TRANSPARENT)
+        canvas.drawBitmap(this, 0F, 0F, null)
+        outputBitmap
+    }catch (e : Exception){
+        null
+    }
 }
 
-fun Bitmap.addPaddingLeftForBitmap( paddingLeft: Int): Bitmap? {
-    val outputBitmap =
-        Bitmap.createBitmap(width + paddingLeft, height, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(outputBitmap)
-    canvas.drawColor(Color.TRANSPARENT)
-    canvas.drawBitmap(this, paddingLeft.toFloat(), 0F, null)
-    return outputBitmap
+fun Bitmap.addPaddingLeftForBitmap(paddingLeft: Int): Bitmap? {
+    return try {
+        val outputBitmap =
+            Bitmap.createBitmap(width + paddingLeft, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(outputBitmap)
+        canvas.drawColor(Color.TRANSPARENT)
+        canvas.drawBitmap(this, paddingLeft.toFloat(), 0F, null)
+        outputBitmap
+    }catch (e : Exception){
+        null
+    }
 }
