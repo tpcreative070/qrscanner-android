a2fc69d585b1dd865bb3c0b7cd441386f7edcb33
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2022-12-27 10:12:01 +0700

    [feature-233705122022] - Improved scanner

---
+++ app/src/main/java/tpcreative/co/qrscanner/ui/guides/GuidesActivity.kt
@@ -0,0 +1,12 @@
+package tpcreative.co.qrscanner.ui.guides
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tpcreative.co.qrscanner.R
+
+class GuidesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac)
+    }
+}
